0458838ab558f422489e9953ad07f6f4e3e1160a
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-09-01 17:10:03 -0700

    Added Find frequency

---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to HashTable!");
+            Console.WriteLine(" Enter 1 to find frequency");
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+            MyMapNode<string, string> hashTable = new MyMapNode<string, string>(5);
+                    hashTable.Add("0", "To");
+                    hashTable.Add("1", "be");
+                    hashTable.Add("2", "or");
+                    hashTable.Add("3", "not");
+                    hashTable.Add("4", "To");
+                    hashTable.Add("5", "be");
+                    hashTable.GetFrequency("be");          
+            
+                    break;
+
+                default:
+                    Console.WriteLine("Enter Valid number");
+                    break;
+            }
+        }
+    }
+}
